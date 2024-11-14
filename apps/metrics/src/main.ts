3b1aff57ba02f501dec30aa8e78465203222237d
anuptripathi <anup.giit@gmail.com>
2024-11-14 13:27:26 +0530

    first working example with telegrafd

--- apps/metrics/src/main.ts
+++ apps/metrics/src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { MetricsModule } from './metrics.module';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(MetricsModule);
+  app.use('/metrics/telegraf', bodyParser.text({ type: 'text/plain' }));
   app.useLogger(app.get(Logger));
   app.setGlobalPrefix('metrics');
   const configService = app.get(ConfigService);
--- apps/metrics/src/metrics.controller.ts
+++ apps/metrics/src/metrics.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { MetricsService } from './metrics.service';
+import { Request, Response } from 'express';
 
 @Controller()
 export class MetricsController {
@@ -8,5 +9,27 @@ export class MetricsController {
   @Get()
   getHello(): string {
     return this.metricsService.getHello();
+  }
+
+  @Post('telegraf')
+  async receiveTelegrafMetrics(
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<void> {
+    try {
+      const metrics = JSON.parse(req.body);
+      console.log(
+        'Received Telegraf Metrics:',
+        JSON.stringify(metrics, null, 2),
+      );
+
+      // Process metrics as needed
+      // You can save them to a database or trigger other services
+
+      res.status(200).send('Metrics received');
+    } catch (error) {
+      console.error('Error processing metrics:', error);
+      res.status(500).send('Failed to process metrics');
+    }
   }
 }
